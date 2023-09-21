83687b062dfe8aad1fd720736c5931bbe3c0b8e8
henry9405 <144359211+henry9405@users.noreply.github.com>
2023-09-21 20:36:44 +0300

    newest in town

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(newBestCustomer){
+    bestCustomer = newBestCustomer;
+}
+const leastFavoriteCustomer = ('steve')
+
+function changeLeastFavoriteCustomer(leastFavoriteCustomer1){
+    leastFavoriteCustomer = leastFavoriteCustomer1;
+}
